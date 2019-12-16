810df9f57e219afd3b4d0c5087332760b18abafc
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-16 18:00:49 -0500

    Implement algorithm, per spec

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace favorite_saying
 {
@@ -6,7 +7,36 @@ namespace favorite_saying
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name = GetInput("What is your name? ");
+            string saying = GetInput("What is your favorite saying? ");
+            WriteLine(DisplayMessage(name, saying));
+        }
+
+        static string GetInput(string label)
+        {
+            Write("{0}", label);
+            string result = ReadLine();
+            return result;
+        }
+
+        static string GenerateLine(int reps)
+        {
+            string result = string.Empty;
+            for (int i = 0; i < reps; i++)
+            {
+                result += String.Format("<>");
+            }
+            return result;
+        }
+
+        static string DisplayMessage(string name, string saying)
+        {
+            string result = string.Empty;
+            result += string.Format("{0}\n", GenerateLine(20));
+            result += String.Format("{0, 18} {1, 18}\n", "Name:", name);
+            result += String.Format("{0, 18} {1, 18}\n", "Favorite saying:", saying);
+            result += string.Format("{0}\n", GenerateLine(20));
+            return result;
         }
     }
 }
