23ee63ed65e0c755d9aa027a1d555615897fe944
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-16 17:30:54 -0500

    Initial commit of favorite-saying project scaffold

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace favorite_saying
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
